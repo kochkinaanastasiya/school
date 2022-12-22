d9624f027362f9a373205279717a07176adb7d25
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2022-12-23 02:42:30 +0300

    Second commit

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,48 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.*;
+
+@Service
+public class FacultyService {
+
+    private final HashMap<Long, Faculty> faculties = new HashMap<>();
+    private long count = 0;
+
+    //Редактировать методы
+    public Faculty addFaculty (Faculty faculty) {
+        faculty.setId(count++);
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty getFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(long id, Faculty faculty) {
+        if (!faculties.containsKey(id)) {
+            return null;
+        }
+        faculties.put(id, faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return faculties.remove(id);
+    }
+
+    public Collection<Faculty> findByColor(String color) {
+        ArrayList<Faculty> result = new ArrayList<>();
+        for (Faculty faculty : faculties.values()) {
+            if (Objects.equals(faculty.getColor(), color)) {
+                result.add(faculty);
+            }
+        }
+        return result;
+    }
+
+}
